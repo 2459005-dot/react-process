a46ed5b8520e352d06ea3243ff209fc03376c2c3
2459005-dot <2459005@kbu.ac.kr>
2025-08-06 13:47:56 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
-import Section from './components/Section'
+import Button from './components/Button'
+import Input from './components/Input'
 function App() {
 
   return (
     <div>
-      <Section/>
-      <h1>hi react</h1>
+      <Input
+        inputValue={'hello react'}
+        title={'input title'}
+        placeholder={'입력하세요'} />
+      <h1>hello React!</h1>
+      <Button text={'mail'} color={'red'}/>
+      <Button text={'cafe'} color={'blue'}/>
+      <Button text={'blog'} color={'green'}/>
+      <Button text={'default'} />
+      <Button text={'자식요소설정'} color={'pink'}>
+        <strong>중요</strong>
+      </Button>      
     </div>
   )
 }
