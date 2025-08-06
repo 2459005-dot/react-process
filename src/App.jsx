5d426c0e8e1e72ef3ceb3689785bae90a7529920
2459005-dot <2459005@kbu.ac.kr>
2025-08-06 12:01:37 +0900

    .

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Section from './components/Section'
+function App() {
+
+  return (
+    <div>
+      <Section/>
+      <h1>hi react</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Section.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Section.css'
+
+const Section = () => {
+    const user = {
+        name: 'Alice',
+        isLogin: false
+    }
+    return (
+        <div>           
+             {user.isLogin ?
+            (<div className='login'>로그인</div>) :
+            (<div className='logout'>로그아웃</div>)
+        }</div>
+    )
+}
+
+export default Section
